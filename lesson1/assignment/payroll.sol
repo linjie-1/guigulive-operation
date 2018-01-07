dfcde3cc13ba9354000a7f4943776cc11b929fe2
old2 <achenge07@163.com>
2018-01-07 17:04:16 +0800

    Update and rename yours.sol to payroll.sol

---
+++ lesson1/assignment/payroll.sol
@@ -0,0 +1,54 @@
+/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll{
+    uint constant payDuration = 10 seconds;
+    
+    address owner;
+    uint salary = 1 ether;
+    address employee = 0x0;
+    uint lastPayday = now;
+    
+    function Payroll(){
+        owner = msg.sender;
+    }
+    
+    function updateSalary(uint s){
+        require(msg.sender == owner);
+        salary = s * 1 ether;
+    }
+    
+    function updateEmployee(address e) {
+        require(msg.sender == owner);
+        
+        if(employee != 0x0){
+            uint payment = salary * (now - lastPayday)/payDuration;
+            employee.transfer(payment);
+        }
+        
+        employee = e;
+        lastPayday = now;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool){
+        return calculateRunway() >= 1;
+    }
+    
+    function getPaid(){
+        require(msg.sender == employee);
+        
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now && hasEnoughFund());
+        
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+}
