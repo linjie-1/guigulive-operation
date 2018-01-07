34c19fcb246974682a80f186d8525e51ac262e17
韩博文 <hanbowen@meituan.com>
2018-01-08 01:05:33 +0800

    提交作业，添加修改员工地址和员工薪资的函数
    增加了modifer，通过了remix静态检查

--- lesson1/assignment/yours.sol
+++ lesson1/assignment/yours.sol
@@ -1,1 +1,66 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    uint salary;
+    address employee;
+    uint lastPayday;
+   
+    
+    function Payroll(address _employee, uint _salary) public{
+        owner = msg.sender;
+        employee = _employee;
+        salary = _salary;
+    }
+    
+    function updateEmployee(address e, uint s)  public {
+        require(msg.sender == owner);
+        
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
+        
+        employee = e;
+        salary = s * 1 ether;
+        lastPayday = now;
+    }
+    
+    function checkAuthority()  private view{
+        require(msg.sender == owner);
+    }
+    
+    function updateEmployeeAddress(address newAddr)  public{
+        checkAuthority();
+        employee = newAddr;
+    }
+    
+    function updateSalary(uint newSalary)  public{
+        checkAuthority();
+        salary = newSalary;
+    }
+    
+    function addFund() public payable returns (uint)  {
+        return this.balance;
+    }
+    
+    function calculateRunway() public view returns (uint)  {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() public view returns (bool)  {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid()  public{
+        require(msg.sender == employee);
+        
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+}
