0f9f69fe16e39ef40c23f9dc4784ea15920d695f
ethangao <lovekobe@gmail.com>
2018-01-07 18:55:39 +0800

    Update yours.sol

--- lesson1/assignment/yours.sol
+++ lesson1/assignment/yours.sol
@@ -1,1 +1,47 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint constant payDuration = 30 days;
+
+    address owner;
+    uint salary = 1 ether;
+    address employee;
+    uint lastPayday;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+    
+    function updateEmployeeAddress(address addr) {
+        require(msg.sender == owner);
+        employee = addr;
+    }
+    
+    function updateEmployeeSalary(uint s) {
+        require(msg.sender == owner);
+        salary = s;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() {
+        require(msg.sender == employee);
+        
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+        assert(hasEnoughFund());
+
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+}
