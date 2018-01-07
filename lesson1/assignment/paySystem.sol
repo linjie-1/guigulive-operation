c0f4e5322edb405796438978cc15fc243f9f9bce
yammyxing <yammyxing@gmail.com>
2018-01-07 17:32:17 +0800

    97-邢亚铭 homework for lesson 1
    hand in my homework

---
+++ lesson1/assignment/paySystem.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    address owner;
+    address employee;
+    uint salary;
+    uint lastPayday;
+    
+    uint constant payDuration = 10 seconds;
+    
+    function ImOwner() public returns (address) {
+        return owner = msg.sender;
+    }
+    
+    function ownerUpdateEmployee(address newAddress, uint newSalary) public {
+        require(msg.sender == owner);
+        
+        employee = newAddress;
+        salary = newSalary;
+        lastPayday = now;
+    }
+    
+    function addMoney() public payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() public returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughMoney() public returns (bool) {
+        return calculateRunway() > 1;
+    }
+    
+    function getPaid() public {
+        require(msg.sender == employee);
+ 
+        uint shouldPayday = lastPayday + payDuration;
+        assert(shouldPayday < now);
+        
+        lastPayday = shouldPayday;
+        employee.transfer(salary);
+    }
+}
