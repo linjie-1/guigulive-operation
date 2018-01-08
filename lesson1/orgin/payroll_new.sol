09b836eb629d8d202113d25a4f29492e34afe6a3
devil63 <497812140@qq.com>
2018-01-09 01:49:02 +0800

    Create payroll_new.sol

---
+++ lesson1/orgin/payroll_new.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    uint salary;
+    address employee;
+    uint lastPayday;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+    
+    function updateEmployeeAddress(address e) returns (address){
+        require(msg.sender == owner);
+        
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
+        
+        employee = e;
+        lastPayday = now;
+        
+        return  employee;
+    }
+    
+    function updateEmployeeSalary(uint s) returns (uint){
+        salary = s * 1 ether;
+        return salary;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() {
+        require(msg.sender == employee);
+        
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+}
