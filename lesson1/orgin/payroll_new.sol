b2624d7822f369e573979f309dc366d6ed01bb72
devil63 <497812140@qq.com>
2018-01-09 23:40:12 +0800

    Update payroll_new.sol

--- lesson1/orgin/payroll_new.sol
+++ lesson1/orgin/payroll_new.sol
@@ -27,6 +27,13 @@ contract Payroll {
     }
     
     function updateEmployeeSalary(uint s) returns (uint){
+        require (msg.sender == owner);
+       
+        uint payment = salary * (now - lastPayday) / payDuration;
+        employee.transfer(payment);
+
+        lastPayday = now;
+        
         salary = s * 1 ether;
         return salary;
     }
