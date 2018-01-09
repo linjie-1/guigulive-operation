1e280bf3b4949364d6c56bc298a279f18bb8b80e
Sean Xiaowen Zhang <seanxwzhang@gmail.com>
2018-01-09 22:22:56 +0800

    changed function signature

--- lesson1/assignment/yours.sol
+++ lesson1/assignment/yours.sol
@@ -1,43 +1,48 @@
-  pragma solidity ^0.4.14;
+pragma solidity ^0.4.14;
 
- contract PayRoll {
-     uint salary = 1 ether;
-     address frank = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
-     uint constant duration = 10 seconds;
-     uint lastpayday = now;
+contract PayRoll {
+    uint salary = 1 ether;
+    address boss = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+    address frank = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c;
+    uint constant duration = 10 seconds;
+    uint lastpayday = now;
 
-     function addFund() payable returns (uint) {
-         return this.balance;
-     }
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
 
-     function calculateRunway() returns (uint) {
-         return this.balance / salary;
-     }
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
 
-     function hasEnoughfund() returns (bool) {
-         return calculateRunway() > 0;
-     }
+    function hasEnoughfund() returns (bool) {
+        return calculateRunway() > 0;
+    }
 
-     function getPaid() {
-         if(msg.sender != frank) {
-             revert();
-         }
-         uint nextPayday = lastpayday + duration;
-         if (nextPayday >= now) {
-             revert();
-         }
-         lastpayday = nextPayday;
-         frank.transfer(salary);
-     }
+    function getPaid() {
+        if(msg.sender != frank) {
+            revert();
+        }
+        uint nextPayday = lastpayday + duration;
+        if (nextPayday >= now) {
+            revert();
+        }
+        lastpayday = nextPayday;
+        frank.transfer(salary);
+    }
 
-     function changeAddress() {
-         frank = msg.sender;
-     }
+    function changeAddress(address addr) {
+        if (msg.sender != boss) {
+            revert();
+        }
+        frank = addr;
+    }
 
-     function changeSalary() {
-         if (msg.value <= 0) {
-             revert();
-         }
-         salary = msg.value;
-     }
- }
+    function changeSalary(uint target) {
+        if (msg.sender != boss || target <= 0) {
+            revert();
+        }
+        salary = target;
+    }
+}
+
