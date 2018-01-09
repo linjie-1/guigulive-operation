90d000908e11f4155ba763a85b992921a500cf37
Xiang Li <bylixiang@gmail.com>
2018-01-08 22:38:56 -0800

    第一课作业

--- lesson1/assignment/yours.sol
+++ lesson1/assignment/yours.sol
@@ -1,1 +1,49 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint salary;
+    address boss;
+    address employee;
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now;
+    
+    function Payroll() {
+        boss = msg.sender;
+    }
+    
+    function updateEmployee(address a, uint s) {
+        if (msg.sender != boss) {
+            revert();
+        }
+        
+        employee = a;
+        salary = s * 1 ether;
+        lastPayday = now;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() {
+        if(msg.sender != employee) {
+            revert();
+        }
+        
+        uint nextPayday = lastPayday + payDuration;
+        if (nextPayday > now) {
+            revert();
+        }
+        
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+}
