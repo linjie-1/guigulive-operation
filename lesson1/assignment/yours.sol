825dd16aa6ce139c49988b70c29407431c6b1c4d
Xinpeng Huang <xph@upstart.com>
2018-01-08 23:44:15 -0800

    Homework 1

--- lesson1/assignment/yours.sol
+++ lesson1/assignment/yours.sol
@@ -1,1 +1,56 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    address employee;
+    uint salary;
+    uint lastPayDay;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+
+    function updateEmployee(address newEmployee) returns (address) {
+        require(msg.sender == owner);
+
+        if (employee != newEmployee) {
+            employee = newEmployee;
+            lastPayDay = now;
+        }
+
+        return employee;
+    }
+
+    function updateSalary(uint newSalary) returns (uint) {
+        salary = newSalary * 1 ether;
+
+        return salary;
+    }
+
+    function addFund() payable returns (uint) {
+        require(msg.sender == owner);
+
+        return this.balance;
+    }
+
+    function calculateRunway () returns (uint) {
+        return this.balance / salary;
+    }
+
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+
+    function getPaid() {
+        require(msg.sender == employee);
+        require(hasEnoughFund());
+
+        uint newPayDay = lastPayDay + payDuration;
+        require(newPayDay < now);
+
+        lastPayDay = newPayDay;
+        employee.transfer(salary);
+    }
+}
