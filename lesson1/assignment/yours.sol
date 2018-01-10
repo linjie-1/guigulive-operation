5d7a173bf29170fc80afd3f401889308f0db4968
Jialiang <wangvincent1993@gmail.com>
2018-01-10 16:07:24 +0800

    split employ setting functions

--- lesson1/assignment/yours.sol
+++ lesson1/assignment/yours.sol
@@ -1,1 +1,67 @@
 /*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    uint salary;
+    address employee;
+    uint lastPayday;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+    
+    function updateEmployeeAddress(address e ){
+        require(msg.sender == owner);
+        
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
+        
+        employee = e;
+        lastPayday = now;
+    }
+    
+    function updateEmployeeSalary(uint s){
+        require(msg.sender == owner);
+        
+        salary = s * 1 ether;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() payable {
+        require(msg.sender == employee);
+        
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+    
+    function getFullPaid() payable{
+        require(msg.sender == employee);
+        
+        uint gaps = (now - lastPayday);
+        uint numPays =  gaps / payDuration;
+        assert(numPays > 0);
+        
+        lastPayday = lastPayday + (numPays * payDuration);
+        uint amount = salary * numPays;
+        employee.transfer(amount);
+    }
+}
