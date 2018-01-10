ae804f0979f89e24588af81f21b5260d41535398
h1994st <h1994st@gmail.com>
2018-01-09 22:46:22 -0500

    Finish assignment 1

--- lesson1/assignment/yours.sol
+++ lesson1/assignment/yours.sol
@@ -1,1 +1,71 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    address owner;
+    uint constant payDuration = 10 seconds;
+
+    // Employee information
+    address employee;
+    uint salary;
+    uint lastPayday;
+
+    function Payroll() {
+        // Assign a contract owner
+        owner = msg.sender;
+    }
+
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+
+    function getPaid() {
+        // Sender should be the employee
+        require(msg.sender == employee);
+
+        // 0. Original version
+        // Check payday information
+        // uint nextPayday = lastPayday + payDuration;
+        // assert(nextPayday < now);
+
+        // Update payday information
+        // lastPayday = nextPayday;
+
+        // Transfer salary
+        // employee.transfer(salary);
+
+        // 1. Updated version
+        // Check number of pay durations
+        uint numPayCycle = (now - lastPayday) / payDuration;
+        assert(numPayCycle >= 1); // At least one duration
+
+        // Update payday information
+        lastPayday += numPayCycle * payDuration;
+
+        // transfer salary
+        employee.transfer(salary * numPayCycle);
+    }
+
+    function updateEmployee(address e, uint s) {
+        // Sender should be the owner
+        require(msg.sender == owner);
+
+        if (employee != 0x0) {
+            // Pay salary before change an existing employee
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
+
+        // Update employee
+        employee = e;
+        salary = s * 1 ether;
+        lastPayday = now;
+    }
+}
