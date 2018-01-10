3d9e009c27632bc19fe0198d0c09ed96f9a62c09
pand <w5pand@163.com>
2018-01-10 21:36:44 +0800

    更改员工和工资标准前先结算一下

--- lesson1/assignment/Payroll.sol
+++ lesson1/assignment/Payroll.sol
@@ -17,14 +17,18 @@ contract Payroll {
     }
 
     //给合约存点钱
-    function addFun() payable returns(uint){
+    function addFun() payable returns (uint){
         return this.balance;
     }
 
-    //换个人发工资
+    //换个人
     function changeEmployee(address _employee) {
-        if (msg.sender != boss) {
-            revert();
+        require(msg.sender == boss && _employee!=0x0);
+        //结清上一个员工工资
+        uint num = caculateSalaryNum();
+        if(employee!=0x0 && num>0){
+            lastPayDay = lastPayDay + payDuration * num;
+            employee.transfer(salary * num);
         }
         employee = _employee;
         lastPayDay = now;
@@ -32,8 +36,12 @@ contract Payroll {
 
     //给个工资标准
     function changeSalary(uint _salary) {
-        if (msg.sender != boss) {
-            revert();
+        require(msg.sender == boss);
+        //按上一个工资标准结清
+        uint num = caculateSalaryNum();
+        if(employee!=0x0 && num>0){
+            lastPayDay = lastPayDay + payDuration * num;
+            employee.transfer(salary * num);
         }
         salary = _salary;
     }
@@ -43,30 +51,29 @@ contract Payroll {
         return this.balance / salary;
     }
 
+    //计算应该领几次工资
+    function caculateSalaryNum() returns (uint) {
+        return (now - lastPayDay) / payDuration;
+    }
+
     //老板还有钱发工资么
     function hasEnouthFound() returns (bool) {
         return caculateRunway() > 0;
     }
 
-    //终于能领钱了
+    //领钱
     //TODO 一无所有的人是不能领钱的，先去借点手续费再说
     function getPaid(){
         //自己领自己的
-        if (employee != msg.sender) {
-            revert();
-        }
-        //老板没钱了
-        if (!hasEnouthFound()) {
-            revert();
-        }
-        //今儿不发钱
-        uint currentPayDay = lastPayDay + payDuration;
-        if (currentPayDay > now) {
-            revert();
-        }
+        require(employee == msg.sender);
+        //老板有钱
+        require(hasEnouthFound());
 
-        lastPayDay = currentPayDay;
-        employee.transfer(salary);
+        uint num = caculateSalaryNum();
+        if (num > 0) {
+            lastPayDay = lastPayDay + payDuration * num;
+            employee.transfer(salary * num);
+        }
     }
 
 }
