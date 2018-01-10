d7b88caa24710db37e03bf4e6b40e2998b8201fa
liuliang <iliang531@gmail.com>
2018-01-10 20:33:55 +0800

    8-陆周-第一课作业

--- 【测试】第一课作业.sol
+++ 【测试】第一课作业.sol
@@ -3,9 +3,11 @@
 pragma solidity ^0.4.14;
 contract Payroll {
     uint salary = 1 ether;
+    // 默认转账地址
     address person = 0xdd870fa1b7c4700f2bd7f44238821c26f7392148;
     uint constant payDuration = 10 seconds;
     uint lastPayday = now;
+    // 设置地址
     function setAddress(address addr) {
         person = addr;
     }
@@ -13,7 +15,7 @@ contract Payroll {
     function addFund() payable returns (uint) {
         return this.balance;
     }
-    
+
     function colculateRunway() returns (uint) {
         return this.balance / salary;
     }
