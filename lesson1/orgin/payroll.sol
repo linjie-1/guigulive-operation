dd271dd4eee0109ad9d9dc614608784b191e7282
Qian Cheng <nicholasqcheng@icloud.com>
2018-01-09 20:27:16 -0800

    Modified homework

--- lesson1/orgin/payroll.sol
+++ lesson1/orgin/payroll.sol
@@ -1,49 +1,95 @@
 pragma solidity ^0.4.14;
 
-contract Payroll {
-    uint constant payDuration = 10 seconds;
+contract SinglePayeePayroll {
+    struct Payee{
+        address payeeAddress;
+        string payeeName;
 
-    address owner;
-    uint salary;
-    address employee;
-    uint lastPayday;
-
-    function Payroll() {
-        owner = msg.sender;
     }
-    
-    function updateEmployee(address e, uint s) {
-        require(msg.sender == owner);
-        
-        if (employee != 0x0) {
-            uint payment = salary * (now - lastPayday) / payDuration;
-            employee.transfer(payment);
+    address owner =  0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+    uint salary = 0;
+    uint constant payDuration = 30 days;
+    uint lastPayday = now;
+    Payee payee;
+
+    function setPayee(string name, address addr) {
+        if(msg.sender != owner) {
+            revert();
         }
-        
-        employee = e;
-        salary = s * 1 ether;
-        lastPayday = now;
+        payee.payeeName = name;
+        payee.payeeAddress = addr;
     }
     
+    function setPayeeName(string name) {
+        if(msg.sender != owner) {
+            revert();
+        }
+        payee.payeeName = name;
+    }
+    
+    function setPayeeAddress(address addr) {
+        if(msg.sender != owner) {
+            revert();
+        }
+        payee.payeeAddress = addr;        
+    }
+    
+    function getPayeeAddress() returns (address){
+        return payee.payeeAddress;
+    }
+    
+    function getPayeeName() returns (string){
+        return payee.payeeName;
+    }
+    
+    function setSalary(uint sal) {
+        if(msg.sender != owner) {
+            revert();
+        }
+        salary = sal;
+    }
+    
+    function getSalary() returns (uint) {
+        return salary;
+    }
+
     function addFund() payable returns (uint) {
         return this.balance;
     }
-    
     function calculateRunway() returns (uint) {
         return this.balance / salary;
     }
     
-    function hasEnoughFund() returns (bool) {
-        return calculateRunway() > 0;
+    function getPaid() {
+        if(msg.sender != payee.payeeAddress) {
+            revert();
+        }
+        uint nextPayday =lastPayday + payDuration;
+        if (nextPayday > now) {
+            revert();            
+        }
+        lastPayday = nextPayday;
+        payee.payeeAddress.transfer(salary);
     }
     
-    function getPaid() {
-        require(msg.sender == employee);
-        
-        uint nextPayday = lastPayday + payDuration;
-        assert(nextPayday < now);
+// tests    
+    function testGetPayeeName() returns (bool) {
+        setPayeeName("Frank");
+        return keccak256(getPayeeName()) == keccak256("Frank");
+    }
+    
+    function testGetPayee() returns(bool){
+        setPayee("Frank", 0xca35b7d915458ef540ade6068dfe2f44e8fa733c);
+        return keccak256(getPayeeName()) == keccak256("Frank") && getPayeeAddress() == 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+    }
+    
+    function testGetSalary() returns (bool) {
+        setSalary(10000);
+        return getSalary() == 10000;
+    }
 
-        lastPayday = nextPayday;
-        employee.transfer(salary);
+    
+    function test() returns (bool) {
+        return 1 wei == 1;
     }
 }
