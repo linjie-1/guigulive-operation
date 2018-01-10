7d99f2f0530f4dc443528c61698a164a76914857
liuliang <iliang531@gmail.com>
2018-01-10 08:57:43 +0800

    第一次作业.
    比视频多一个设置地址的功能

---
+++ 【测试】第一课作业.sol
@@ -0,0 +1,38 @@
+
+// 第一课作业
+pragma solidity ^0.4.14;
+contract Payroll {
+    uint salary = 1 ether;
+    address person = 0xdd870fa1b7c4700f2bd7f44238821c26f7392148;
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now;
+    function setAddress(address addr) {
+        person = addr;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function colculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function get() returns (uint)  {
+        return this.balance;
+    }
+
+    function hasEnoughFund() returns (bool) {
+        return colculateRunway() > 0;
+    }
+    // 
+    function getPaid() {
+        uint nextPayDay = lastPayday + payDuration;
+        if(nextPayDay < now) {
+            lastPayday = nextPayDay;
+            person.transfer(salary);
+        }
+      
+    }
+
+}
