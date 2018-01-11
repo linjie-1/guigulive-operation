3535e65112eaa89d5413b026b03ba97c8d5c6a69
Zihan Guo <zguocollege@gmail.com>
2018-01-11 10:54:28 +0800

    55_郭子汉 第一次作业(修改后)

--- lesson1/assignment/55_郭子汉.sol
+++ lesson1/assignment/55_郭子汉.sol
@@ -19,6 +19,10 @@ contract Payroll {
     
     function changeSalary(uint newsalary) public {
         require(msg.sender == owner);
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
         salary = newsalary * 1 ether;
     }
     
