795e6acde3a27593e7047a3accc9fb89aec0efee
beijibeijing <34858346+beijibeijing@users.noreply.github.com>
2018-01-11 19:49:12 +0800

    优化calculateRunway以及优化后的花费记录

--- Lesson2/assignment/yours.sol
+++ Lesson2/assignment/yours.sol
@@ -1,7 +1,7 @@
 /*作业请提交在这个目录下*/
-//未优化前 \
+//优化前 
 //创建花费       交易769157  执行542217
-//存100ETH      交易21918   执行646
+//存100ETH       交易21918   执行646
 //加入第1位员工  交易105198  执行82326
 //计算次数       交易22966   执行1694
 //加入第2位员工  交易91039   执行68167
@@ -23,6 +23,36 @@
 //加入第10位员工 交易97767   执行74895
 //计算次数       交易29995   执行8723
 
+//优化后
+//创建花费       交易789817  执行557829   
+//存100ETH       交易21918   执行646
+//加入第1位员工  交易125439  执行102567
+//计算次数       交易22356   执行1084
+//加入第2位员工  交易96280   执行73408
+//计算次数       交易22356   执行1084
+//加入第3位员工  交易97121   执行74249
+//计算次数       交易22356   执行1084
+//加入第4位员工  交易97962   执行75090
+//计算次数       交易22356   执行1084
+//加入第5位员工  交易98803   执行75931
+//计算次数       交易22356   执行1084
+//加入第6位员工  交易99644   执行76772
+//计算次数       交易22356   执行1084
+//加入第7位员工  交易100485  执行77613
+//计算次数       交易22356   执行1084
+//加入第8位员工  交易101326  执行78454
+//计算次数       交易22356   执行1084
+//加入第9位员工  交易102167  执行79295
+//计算次数       交易22356   执行1084
+//加入第10位员工 交易103008  执行80136
+//计算次数       交易22356   执行1084
+
+
+//计算支付次数时，优化前每多一位员工，多花费781 gas，因为每次都要多做一次加法运算；
+//优化后每次花费保持恒定；
+//优化后每次增加员工比优化前都多花费5241 gas,因为比优化前多了计算总工资的步骤；
+//在第7位员工时，优化后节省的gas超过5241 gas。
+
 pragma solidity ^0.4.14;
 
 contract Payroll {
@@ -36,6 +66,7 @@ contract Payroll {
 
     address owner;
     Employee[] employees;
+    uint totalSalary;//优化calculateRunway修改
 
     function Payroll() public {
         owner = msg.sender;
@@ -59,7 +90,10 @@ contract Payroll {
         var (employee, ) = _findEmployee(employeeId);
         assert(employee.id == 0x0);
         
-        employees.push(Employee(employeeId, salary * 1 ether, now));
+        //优化calculateRunway修改
+        salary = salary * 1 ether;
+        employees.push(Employee(employeeId, salary, now));
+        totalSalary += salary;
     }
     
     function removeEmployee(address employeeId) public {
@@ -67,7 +101,8 @@ contract Payroll {
         var (employee, index) = _findEmployee(employeeId);
         assert(employee.id != 0x0);
         
-         _partialPaid(employee);
+        _partialPaid(employee);
+        totalSalary -= employee.salary;//优化calculateRunway修改
         delete employees[index];
         employees[index] = employees[employees.length - 1];
         employees.length -= 1;
@@ -79,19 +114,21 @@ contract Payroll {
         assert(employee.id != 0x0);        
         
         _partialPaid(employee);
+
+        //优化calculateRunway修改
+        totalSalary -= employees[index].salary;
         employees[index].salary = salary * 1 ether;
+        totalSalary += employees[index].salary;
         employees[index].lastPayday = now;
     }
     
     function addFund() public payable returns (uint) {
         return this.balance;
     }
-
+    
+    //优化calculateRunway
     function calculateRunway() public constant returns (uint) {
-        uint totalSalary = 0;
-        for (uint i = 0; i < employees.length; i++) {
-            totalSalary += employees[i].salary;
-        }
+        require(totalSalary != 0);
         return this.balance / totalSalary;
     }
 
