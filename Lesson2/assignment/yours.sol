856c5428da382afe83f8e982861b69a96cc7966d
Tianyi Xing <tianyi.xing@asu.edu>
2018-01-11 16:57:53 -0800

    first update of hw 2

--- Lesson2/assignment/yours.sol
+++ Lesson2/assignment/yours.sol
@@ -1,1 +1,74 @@
 /*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    struct Employee {
+        address id;
+        uint salary;
+        uint lastPayday;
+    }
+    
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    Employee[] employees;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+    
+    function _partialPaid(Employee employee) private {
+        
+        if(employee.id!=0x0){
+            uint toPaid = employee.salary * (now - employee.lastPayday)/payDuration;
+            employee.id.transfer(toPaid);
+        }
+    }
+    
+    function _findEmployee(address employeeId) private returns (Employee, uint) {
+        for(uint i=0; i<employees.length; i++){
+            if(employees[i].id == employeeId){
+                return (employees[i], i);
+            }
+        }
+    }
+
+    function addEmployee(address employeeId, uint salary) {
+        
+    }
+    
+    function removeEmployee(address employeeId) {
+    }
+    
+    function updateEmployee(address employeeId, uint salary) {
+        
+        var (employee, index) = _findEmployee(employeeId);
+        
+        _partialPaid(employee);
+        
+        employee.id = employeeId;
+        employee.lastPayday = now;
+        employee.salary = salary;
+        
+        
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        uint totalSalary = 0;
+       for (uint i = 0; i < employees.length; i++) {
+            totalSalary += employees[i].salary;
+        }
+        return this.balance / totalSalary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() {
+    }
+}
