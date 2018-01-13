8f0e7b1bad34374044d9f170cba0aac2bc6adaa0
sophiemarceauqu <sophiemarceau_qu@sina.com>
2018-01-13 12:42:51 +0800

    Update yours.sol
    第二次课课后作业 提交

--- Lesson2/assignment/yours.sol
+++ Lesson2/assignment/yours.sol
@@ -7,28 +7,28 @@ contract Payroll {
         uint salary;
         uint lastPayday;
     }
-
+    
     uint constant payDuration = 10 seconds;
     uint totalSalary ;
     address owner;
     Employee[] employees;
-
+    
 
     function Payroll() {
         owner = msg.sender;
     }
-
+    
     function _partialPaid(Employee employee) private {
         uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
         employee.id.transfer(payment);
     }
-
+    
     function _findEmployee(address employeeId) private returns (Employee , uint) {
         for(uint i;i < employees.length;i++){
             if(employees[i].id == employeeId){
                 return(employees[i],i);
             }
-        }
+        }    
     }
 
     function addEmployee(address employeeId, uint salary) {
@@ -42,15 +42,15 @@ contract Payroll {
 
     function removeEmployee(address employeeId) {
         require (msg.sender == owner);
-        var (employee,index)= _findEmployee(employeeId);
+        var (employee,index) = _findEmployee(employeeId);
         assert(employee.id != 0x0 );
         _partialPaid(employee);
         delete employees[index];
         totalSalary -= employee.salary;
-        employees[index]=employees[employees.length-1];
-        employees.length-=1;
+        employees[index] = employees[employees.length-1];
+        employees.length -= 1;
     }
-
+    
     function updateEmployee(address employeeId, uint salary) {
         require (msg.sender == owner);
         var (employee,index) = _findEmployee(employeeId);
@@ -59,14 +59,15 @@ contract Payroll {
         uint sSalary = salary * 1 ether;
         employees[index].salary = sSalary;
         employees[index].lastPayday = now;
-        totalSalary -= employee.salary + sSalary;
+        totalSalary -= employee.salary;
+        totalSalary += sSalary;
     }
-
+    
     function addFund() payable returns (uint) {
          require (msg.sender == owner);
          return this.balance;
     }
-
+    
 
     function calculateRunway()public view returns (uint) {
         //因为每次都循环会很费气
@@ -76,26 +77,30 @@ contract Payroll {
     //     }
         return this.balance / totalSalary;
     }
-
-
+    
+    
     function emplength() public view returns(uint){
         return  employees.length;
     }
-
+    
     function balanceOf() public view returns(uint){
         return this.balance;
     }
-
+    
     function hasEnoughFund() returns (bool) {
         return calculateRunway() > 0;
     }
-
+    
     function getPaid() {
         var (employee,index)= _findEmployee(msg.sender);
         assert(employee.id!=0x0);
         uint nextPayday = employee.lastPayday + payDuration;
         assert(now>nextPayday);
         employees[index].lastPayday = nextPayday;
-        employee.id.transfer(employee.salary);
+        employee.id.transfer(employee.salary);  
     }
 }
+
+//说明优化前的每次调calcuateRunway 函数由于每次都循环遍历去很消耗的gas多
+//优化后 每次新添加新员工后调用calcuateRunway 函数所消耗的gas 看了一下日志都是 874
+
