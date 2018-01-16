70ba5a0c03879ac2410da14a5cfc2c71b72619b5
Huang Minzhi <sdnion@gmail.com>
2018-01-17 01:58:06 +0900

    update  snapshot and c3 Linearization

--- Lesson3/assignment/25-黄敏之.sol
+++ Lesson3/assignment/25-黄敏之.sol
@@ -85,3 +85,45 @@ contract Payroll {
     }
 }
 
+/*
+函数截图调用顺序：
+addFund -> addFund.png
+addEmployee 地址1 -> addEmployee.png
+calculateRunway -> calculateRunway.png
+getPaid 地址1 -> getPaid1.png
+hasEnoughFund -> hasEnoughFund.png
+changePaymentAddress 地址1到地址2 -> changePaymentAddress.png
+getPaid 地址2 -> getPaid2.png
+updateEmployee 地址2到地址3 -> updateEmployee.png
+getPaid 地址3 -> getPaid3.png
+
+- 第三题（加分题）：自学C3 Linearization, 求以下 contract Z 的继承线
+- contract O
+- contract A is O
+- contract B is O
+- contract C is O
+- contract K1 is A, B
+- contract K2 is A, C
+- contract Z is K1, K2
+
+L(0) = [0]
+L(A) = [A, O]
+L(B) = [B, O]
+L(C) = [C, O]
+
+L(K1) = [K1] + merge(L[A] + L[B] + [A, B])
+      = [K1] + merge([A, O] + [B, O] + [A, B])
+	  = [K1, A] + merge([O] + [B, O] + [B])
+	  = [K1, A, B] + merge([O], [O])
+	  = [K1, A, B, O]
+L(K2) = [K2, A, C, O]
+
+L(Z) = [Z] + merge(L[K1] + L[K2] + [K1, K2])
+     = [Z] + merge([K1, A, B, O] + [K2, A, C, O] + [K1, K2])
+	 = [Z, K1] + merge([A, B, O] + [K2, A, C, O] + [K2])
+	 = [Z, K1, K2] + merge([A, B, O] + [A, C, O])
+	 = [Z, K1, K2, A] + merge([B, O] + [C, O])
+	 = [Z, K1, K2, A, B] + merge([O] + [C, O])
+	 = [Z, K1, K2, A, B, C] + merge([O], [O])
+	 = [Z, K1, K2, A, B, C, O]
+*/
