97d82bfbe276eff6699afa1040d0a6a3a916d464
achen <achenge07@163.com>
2018-01-16 17:23:21 +0800

    修改调用顺序

--- lesson3/assignment/C3Linearization_py2.7.py
+++ lesson3/assignment/C3Linearization_py2.7.py
@@ -9,10 +9,10 @@ A = Type('A', (object,), {})
 B = Type('B', (object,), {})
 C = Type('C', (object,), {})
 
-K1 = Type('K1', (A, B), {})
-K2 = Type('K2', (A, C), {})
+K1 = Type('K1', (B, A), {})
+K2 = Type('K2', (C, A), {})
 
-Z = Type('Z', (K1, K2), {})
+Z = Type('Z', (K2, K1), {})
 
 print Z.mro()
 
