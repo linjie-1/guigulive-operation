9fb1b35223d9eaa70bebfeb8b76bf2cf7e024644
ZHH <zhh@umich.edu>
2018-01-17 01:01:18 -0500

    ASSIGNMENT3: Implement a somewhat complete contract

--- Lesson3/assignment/README.md
+++ Lesson3/assignment/README.md
@@ -12,3 +12,43 @@
 - contract K1 is A, B
 - contract K2 is A, C
 - contract Z is K1, K2
+
+### Solutions
+
+#### Problem 1
+- Call addEmployee("0x14723a09acff6d2a60dcdf7aa4aff308fddc160c",1):
+	![](https://github.com/mblsktxy/guigulive-operation/blob/master/Lesson3/assignment/images/addEmployee.png)
+- Call addFund():
+	![](https://github.com/mblsktxy/guigulive-operation/blob/master/Lesson3/assignment/images/addFund.png)
+- Call calculateRunway():
+ 	![](https://github.com/mblsktxy/guigulive-operation/blob/master/Lesson3/assignment/images/calculateRunway.png)
+- Call getPaid():
+	![](https://github.com/mblsktxy/guigulive-operation/blob/master/Lesson3/assignment/images/getPaid.png)
+- Call hasEnoughFund():
+	![](https://github.com/mblsktxy/guigulive-operation/blob/master/Lesson3/assignment/images/hasEnoughFund.png)
+- Call removeEmployee("0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db") after addEmployee("0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db",1) is called:
+	![](https://github.com/mblsktxy/guigulive-operation/blob/master/Lesson3/assignment/images/removeEmployee.png)
+- Call updateEmployee("0xca35b7d915458ef540ade6068dfe2f44e8fa733c",2):
+	![](https://github.com/mblsktxy/guigulive-operation/blob/master/Lesson3/assignment/images/updateEmployee.png)
+
+#### Problem 2
+
+Please refer to the code
+
+#### Problem 3
+
+L(O)  := [O]
+L(A)  := [A] + merge(L(O), [O])
+       = [A] + merge([O], [O])
+       = [A, O]
+L(B)  := [B, O]
+L(C)  := [C, O]
+L(K1) := [K1] + merge(L(A), L(B), [A, B])
+       = [K1] + merge([A, O], [B, O], [A, B])
+       = [K1, A, B, O]
+L(K2) := [K2] + merge(L(A), L(C), [A, C])
+       = [K2] + merge([A, O], [C, O], [A, C])
+       = [K2, A, C, O]
+L(Z)  := [Z] + merge(L(K1), L(K2), [K1, K2])
+       = [Z] + merge([K1, A, B, O], [K2, A, C, O], [K1, K2])
+       = [Z, K1, K2, A, B, C, O]
---
+++ Lesson3/assignment/payroll.sol
@@ -0,0 +1,83 @@
+pragma solidity ^0.4.14;
+
+import './SafeMath.sol';
+import './Ownable.sol';
+
+contract Payroll is Ownable {
+    using SafeMath for uint;
+    struct Employee {
+        address id;
+        uint salary;
+        uint lastPayday;
+    }
+
+    uint constant payDuration = 10 seconds;
+    uint totalSalary = 0;
+    mapping(address => Employee) public employees;
+    
+    modifier employeeExist(address employeeId) {
+        var employee = employees[employeeId];
+        assert(employee.id != 0x0);
+        _;
+    }
+    
+    function _partialPaid(Employee employee) private {
+        uint payment = employee.salary
+                       .mul(now.sub(employee.lastPayday))
+                       .div(payDuration);
+        employee.id.transfer(payment);
+    }
+
+    function addEmployee(address employeeId, uint salary) onlyOwner {
+        var employee = employees[employeeId];
+        assert(employee.id == 0x0);
+        
+        employees[employeeId] = Employee(employeeId, salary.mul(1 ether), now);
+        totalSalary = totalSalary.add(employees[employeeId].salary);
+    }
+
+    function removeEmployee(address employeeId) onlyOwner employeeExist(employeeId) {
+        var employee = employees[employeeId];
+        _partialPaid(employee);
+        totalSalary = totalSalary.sub(employees[employeeId].salary);
+        delete employees[employeeId];
+    }
+
+    function updateEmployee(address employeeId, uint salary) onlyOwner employeeExist(employeeId) {
+        var employee = employees[employeeId];
+
+        _partialPaid(employee);
+        totalSalary = totalSalary.sub(employees[employeeId].salary);
+        employees[employeeId].salary = salary.mul(1 ether);
+        employees[employeeId].lastPayday = now;
+        totalSalary = totalSalary.add(employees[employeeId].salary);
+    }
+
+    function changePaymentAddress(address employeeId, address newEmployeeId) onlyOwner employeeExist(employeeId) {
+        var employee = employees[employeeId];
+        employees[newEmployeeId] = Employee(newEmployeeId, employee.salary, employee.lastPayday);
+        delete employees[employeeId];
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+
+    function calculateRunway() returns (uint) {
+        return this.balance.div(totalSalary);
+    }
+
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() employeeExist(msg.sender) {
+        var employee = employees[msg.sender];
+        uint nextPayday = employee.lastPayday.add(payDuration);
+        assert(nextPayday < now);
+        assert(this.balance > employee.salary);
+
+        employees[msg.sender].lastPayday = nextPayday;
+        employee.id.transfer(employee.salary);
+    }
+}
