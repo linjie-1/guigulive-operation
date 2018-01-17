c9332ab7783ba02ca3679f9a6911ace24b2d1f0e
yiyunchi <yiyunchi@meituan.com>
2018-01-17 11:11:38 +0800

    修复 findEmployee 没有默认返回值问题

--- lesson2/EnhancedPayroll.sol
+++ lesson2/EnhancedPayroll.sol
@@ -7,28 +7,30 @@ contract EnhancedPayroll {
         uint salaryInMonth;
         uint lastPayDay;
     }
-    
+
     uint constant payDuration = 10 seconds;
     address owner;
     Employee[] employees;
-    
+
     function EnhancedPayroll() {
         owner = msg.sender;
     }
-    
+
     function _partialPaid(Employee employee) private {
         uint value = employee.salaryInMonth * (now - employee.lastPayDay) / payDuration;
         employee.id.transfer(value);
     }
-    
+
     function _findEmployee(address employeeId) private returns(Employee, uint) {
         for (uint i = 0; i < employees.length; i++) {
             if (employees[i].id == employeeId) {
                 return (employees[i], i);
             }
         }
+
+        return (Employee(0, 0, 0), -1);
     }
-    
+
     // add
     function addEmployee(address id, uint salary) {
         require(msg.sender == owner);
@@ -37,7 +39,7 @@ contract EnhancedPayroll {
         employees.push(Employee(id, salary * 1 ether, now));
         totalSalary += salary;
     }
-    
+
     // remove
     function removeEmployee(address id) {
         require(msg.sender == owner);
@@ -49,10 +51,10 @@ contract EnhancedPayroll {
         employees[index] = employees[employees.length - 1];
         employees.length -= 1;
     }
-    
+
     uint totalSalary = 0;
-    
-    // update 
+
+    // update
     function updateEmployee(address id, uint salary) {
         require(msg.sender == owner);
         var (employee, index) = _findEmployee(id);
@@ -62,7 +64,7 @@ contract EnhancedPayroll {
         employee.id = id;
         employee.salaryInMonth = salary;
     }
-    
+
     // addFund
     function addFund() payable returns (uint) {
         return this.balance;
@@ -89,4 +91,3 @@ contract EnhancedPayroll {
         employee.id.transfer(employee.salaryInMonth);
     }
 }
-
