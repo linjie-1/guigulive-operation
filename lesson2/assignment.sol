0fa5cf52275179adf434a9a4e94acd89720d6f3d
韩博文 <hanbowen@meituan.com>
2018-01-17 23:22:44 +0800

    修正第二课作业

--- lesson2/assignment.sol
+++ lesson2/assignment.sol
@@ -8,6 +8,7 @@ contract Payroll {
     uint constant payDuration = 10 seconds;
     address owner;
     Employee[] employees;
+
     uint totalSalary = 0;
     
     function Payroll() public  {
@@ -16,12 +17,13 @@ contract Payroll {
     
     function _partialPaid(Employee employee) private {
         uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
-        assert(payment > 0);
-        employee.lastPayday = now;
-        employee.id.transfer(payment);
+        if(payment > 0){
+            employee.lastPayday = now;
+            employee.id.transfer(payment);
+        }
     }
     
-    function _findEmployee(address employeeId) private returns(Employee, uint) {
+    function _findEmployee(address employeeId) private view returns(Employee, uint) {
         assert(employeeId != 0x0);
         for (uint i = 0; i < employees.length; i++) {
             if (employees[i].id == employeeId) {
@@ -31,12 +33,13 @@ contract Payroll {
         return (Employee(0x0, 0, 0), 0);
     }
     
-    function requireOwner() private {
+    modifier requireOwner() {
         require(msg.sender == owner);
+        _;
     }
     
-    function addEmployee(address employeeId, uint salary) public {
-        requireOwner();
+    function addEmployee(address employeeId, uint salary) public requireOwner{
+        
         for (uint i = 0; i < employees.length; i++) {
             if (employees[i].id == employeeId) {
                 revert();
@@ -46,42 +49,43 @@ contract Payroll {
         employees.push(Employee(employeeId, salary, now));
     }
     
-    function removeEmployee(address employeeId) public {
-        requireOwner();
+    function removeEmployee(address employeeId) public payable requireOwner{
         var (e, index) = _findEmployee(employeeId);
         if (e.id == 0x0) revert();
         _partialPaid(e);
         totalSalary-= e.salary;
         delete employees[index];
-        for (uint i = index; i < employees.length; i++) {
-            employees[i] = employees[i + 1];
+        
+        for (uint i = index+1; i < employees.length; i++) {
+            employees[i-1] = employees[i];
         }
         employees.length--;
 
     }
     
-    function updateEmployee(address employeeId, uint salary) public {
-        requireOwner();
+    function updateEmployee(address employeeId, uint salary) public requireOwner payable{
         var (e, index) = _findEmployee(employeeId);
         if (e.id == 0x0) revert();
         employees[index].salary = salary;
     }
-    
-    function addFund() payable  returns(uint)  {
+
+
+    function addFund() public payable returns(uint) 
+    {
         return this.balance;
     }
     
     
-    function calculateRunway() returns(uint) {
+    
+    function calculateRunway() public view returns(uint) {
        return totalSalary;
     }
     
-    function hasEnoughFund() returns(bool) {
+    function hasEnoughFund() public  view returns(bool) {
         return calculateRunway() > 0;
     }
     
-    function getPaid(Employee e) internal {
-        requireOwner();
+    function getPaid(Employee e) internal requireOwner{
         var nextPayday = e.lastPayday + payDuration;
         assert(nextPayday < now);
         e.lastPayday = nextPayday;
