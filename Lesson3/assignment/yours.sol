0b6eb8345c7bca5a5e9c7b5178a89d40d0e689d7
Xinpeng Huang <masterxph@gmail.com>
2018-01-18 22:11:38 -0800

    Correct linearization

--- Lesson3/assignment/yours.sol
+++ Lesson3/assignment/yours.sol
@@ -1,4 +1,4 @@
-/* 
+/*
 C3 Linearization Problem:
 contract O
 => O
@@ -20,8 +20,8 @@ contract Z is K1, K2
 => Z + merge(L[K2], L[K1], [K2, K1])
    Z + merge([K2, C, A, O], [K1, B, A, O], [K2, K1])
    [Z, K2] + merge([C, A, O], [K1, B, A, O], [K1])
-   [Z, K2, K1] + merge([C, A, O], [B, A, O])
-   [Z, K2, K1, C, B, A, O]
+   [Z, K2, C] + merge([A, O], [K1, B, A, O])
+   [Z, K2, C, K1, B, A, O]
 */
 
 pragma solidity ^0.4.14;
@@ -32,18 +32,18 @@ contract Payroll {
         uint salary;
         uint lastPayday;
     }
-    
+
     uint constant payDuration = 10 seconds;
     uint totalSalary;
 
     address owner;
     mapping(address => Employee) public employees;
-    
+
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
-    
+
     modifier onlyEmployee {
         Employee employee = employees[msg.sender];
         assert(employee.id != 0x0);
@@ -55,76 +55,76 @@ contract Payroll {
         assert(employee.id != 0x0);
         _;
     }
-    
+
     function Payroll() {
         owner = msg.sender;
     }
-    
+
     function _partialPaid(Employee employee) private {
         uint partialPayment = employee.salary * (now - employee.lastPayday) / payDuration;
         assert(this.balance >= partialPayment);
-        
+
         employee.lastPayday = now;
         employee.id.transfer(partialPayment);
     }
-    
+
     function addEmployee(address employeeId, uint salary) onlyOwner {
         Employee employee = employees[employeeId];
         assert(employee.id == 0x0);
-        
+
         salary = salary * 1 ether;
         employees[employeeId] = Employee(employeeId, salary, now);
         totalSalary += salary;
     }
-    
+
     function removeEmployee(address employeeId) onlyOwner employeeExists(employeeId) {
         Employee employee = employees[employeeId];
 
         _partialPaid(employees[employeeId]);
-        
+
         totalSalary -= employee.salary;
         delete employees[employeeId];
     }
-    
+
     function updateEmployee(address employeeId, uint salary) onlyOwner employeeExists(employeeId) {
         Employee employee = employees[employeeId];
 
         _partialPaid(employees[employeeId]);
-        
+
         uint newSalary = salary * 1 ether;
         totalSalary += newSalary - employee.salary;
         employees[employeeId].salary = newSalary;
     }
-    
+
     function changeEmployeeAddress(address newEmployeeId) onlyEmployee {
         Employee employee = employees[msg.sender];
-        
+
         employee.id = newEmployeeId;
         employees[newEmployeeId] = employee;
         delete employees[msg.sender];
     }
-    
+
     function addFund() payable returns (uint) {
         require(msg.sender == owner);
-        
+
         return this.balance;
     }
-    
+
     function calculateRunway() returns (uint) {
         return this.balance / totalSalary;
     }
-    
+
     function hasEnoughFund() returns (bool) {
         return calculateRunway() > 0;
     }
-    
+
     function getPaid() onlyEmployee returns (Employee) {
         Employee employee = employees[msg.sender];
 
         uint nextPayday = employee.lastPayday + payDuration;
         assert(nextPayday < now);
         assert(this.balance >= employee.salary);
-        
+
         employees[msg.sender].lastPayday = nextPayday;
         employee.id.transfer(employee.salary);
     }
