08dd48cbd216af8421d481f2be5ed6974d81866d
陈扬帆 <cyf3211@qq.com>
2018-01-19 21:01:17 +0800

    修改附加题

--- Lesson3/assignment/yours.sol
+++ Lesson3/assignment/yours.sol
@@ -9,10 +9,25 @@ function changePaymentAddress(address employeeId, address newEmployeeId) onlyOwn
 }
 
 //加分题
-O[O],
-A[A,O],
-B[B,O],
-C[C,O],
-K1[K1,A,B,O],
-K2[K2,A,C,O],
-Z[K1,K2,A,B,C,O]
+L[O] = 0
+
+L[A] = A + merge[L[0],0] 
+     = [A,0]
+
+L[B] = B + merge[L[0],0] 
+     = [B,0]
+
+L[C] = C + merge[L[0],0] 
+     = [C,0]
+
+L[K1] = K1 + merge[[A,0],[B,0],[A,B]]
+      = [K1,B] + merge[[A,0],[0],[A]]
+      = [K1,B,A,0]
+      
+L[K2] = K2 + merge[[A,0]],[C,0]],[A,C]]
+      = [K2,C,A,0]
+
+L[Z] = Z + merge[[K1,B,A,0],[K2,C,A,0],[K1,K2]]     
+     = [Z,K2] + merge[[K1,B,A,0],[C,A,0],K1]
+     = [Z,K2,K1] + merge[[B,A,0],[C,A,0]]
+     = [Z,K2,K1,A,C,B,0]
