14b9d9623811c1bd0de006127f99d4e4f2bc2e61
yexu15 <yexu15@gmail.com>
2018-01-20 05:47:16 -0800

    submit HW4 unittest

---
+++ Lesson4/assignment/testPayroll.js
@@ -0,0 +1,42 @@
+var Payroll = artifacts.require("./Payroll.sol");
+
+contract('Payroll', function(accounts) {
+      
+    it("check add employee", function() {
+        Payroll.deployed().then(function(instance) {
+            return instance.addEmployee(accounts[1],1);
+        }).then(function() {
+            assert(true, "pass the test.");
+        })
+    });
+
+    it("check if the nonowner can call addEmployee", function() {
+        Payroll.deployed().then(function(instance) {
+            return instance.addEmployee(accounts[1],1,{from:accounts[3]});
+        }).catch(function(error) {
+            assert(error.toString().includes('revert'), "exception !");
+        });
+    });
+
+    it("check if the removed employee is actually in employees", function() {
+        Payroll.deployed().then(function(instance) {
+            payrollInstance = instance;
+            return payrollInstance.removeEmployee(accounts[1]);
+        }).then(function() {
+            assert(error.toString().includes('invalid opcode'), "exception !");
+        })
+    });
+
+
+
+    it("check if the nonowner can call removeEmployee", function() {
+        Payroll.deployed().then(function(instance) {
+            payrollInstance = instance;
+            return payrollInstance.addEmployee(accounts[1],1);
+        }).then(function() {
+            return payrollInstance.removeEmployee(accounts[1],{from:accounts[1]});
+        }).catch(function(error) {
+            assert(error.toString().includes('invalid opcode'), "exception !");
+        })
+    });
+});
