872af23030b88e875b4c141176633a7c5049dfbd
Fang Gong <fang.ucla@gmail.com>
2018-01-20 13:47:22 +0000

    Update payroll.js

--- Lesson4/assignment/payroll.js
+++ Lesson4/assignment/payroll.js
@@ -3,7 +3,7 @@ var payroll = artifacts.require("./payroll.sol");
 contract('payroll', function(accounts) {
 
 
-  // add a new employee
+  // 1.0 add a new employee
   it("...should add a new employee.", function() {
     let employeeId = accounts[1];
     let salary = 1;  
@@ -22,7 +22,7 @@ contract('payroll', function(accounts) {
     });
   });
 
-  // should not add a new employee by other employee
+  // 1.1 should not add a new employee by non-owner person
   it("...should not add a new employee by non-owner person.", function() {
     let employeeId = accounts[1];
     let salary = 1;  
@@ -30,15 +30,17 @@ contract('payroll', function(accounts) {
 
     return payroll.deployed().then(function(instance) {
       payrollInstance = instance;
-
+      // employee tries to add self -> it is an error!
       return payrollInstance.addEmployee(employeeId, salary, {from: employeeId});
     }).catch(function(error) {
-        assert(error);
+        assert.throws(function() {
+            throw error;
+        }, 'Exception');
     });
   });
 
 
-  // should not add an existed employee
+  // 1.2 should not add an existed employee
   it("...should not add an exisetd employee.", function() {
     let employeeId = accounts[1];
     let salary = 1;  
@@ -46,19 +48,21 @@ contract('payroll', function(accounts) {
 
     return payroll.deployed().then(function(instance) {
       payrollInstance = instance;
-      // add record of employeeId -> then he/she has become an employee
+      // add record of employeeId -> then he/she becomes an employee
       payrollInstance.employees[employeeId] = {id: employeeId, salary: salary};
-      // add the same employee again -> wrong!
+      // add the same employee again -> it is an error!
       return payrollInstance.addEmployee(employeeId, salary, {from: owner});
     }).catch(function(error) {
-        assert(error);
+        assert.throws(function() {
+            throw error;
+        }, 'Exception');
     });
   });
 
 
 
 
-  // remove employee with expected behavior: add employee first and then remove
+  // 2.0 remove employee with expected behavior: add employee first and then remove
   it("...should remove the target employee.", function() {
     let employeeId = accounts[1];
     let salary = 1;  
@@ -66,22 +70,22 @@ contract('payroll', function(accounts) {
 
     return payroll.deployed().then(function(instance) {
       payrollInstance = instance;
-
+      // owner add employee first
       return payrollInstance.addEmployee(employeeId, salary, {from: owner});
     }).then(function() {
-        // remove this employee
+        // then remove this employee
       return payrollInstance.removeEmployee(employeeId, {from: owner});
     }).then(function() {
         // access record of this removed employee
         return payrollInstance.employees.call(employeeId);
       }).then(function(employee) {
-          // non-existed employee: address is default value "0x0"
+          // non-existed employee: address should be default value "0x0"
           assert.equal(employee[0], "0x0", "non-existed employee");
           assert(true);
       });
   });
 
-  // remove employee by person other than owner
+  // 2.1 remove employee by non-owner person
   it("...should not remove employee by non-owner person.", function() {
     let employeeId = accounts[1];
     let salary = 1;  
@@ -95,14 +99,16 @@ contract('payroll', function(accounts) {
       // let employee to remove self is an error!
       return payrollInstance.removeEmployee(employeeId, {from: employeeId});
     }).catch(function(error) {
-        assert(error);
+        assert.throws(function() {
+            throw error;
+        }, 'Exception');
     });
   });
 
-  // remove non-existed employee
+  // 2.2 remove non-existed employee
   it("...should not remove non-existed employee.", function() {
     let owner = accounts[0];
-    // accounts[2] has never been added as employee, try to remove is an error
+    // accounts[2] has never been added as employee, try to remove him/her is an error!
     let employee = accounts[2];
 
     return payroll.deployed().then(function(instance) {
@@ -110,7 +116,9 @@ contract('payroll', function(accounts) {
 
       return payrollInstance.removeEmployee(employee, {from: owner});
     }).catch(function(error) {
-        assert(error);
+        assert.throws(function() {
+            throw error;
+        }, 'Exception');
     });
   });
 
