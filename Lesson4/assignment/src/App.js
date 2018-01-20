af19764498866490d89b1389e1c08256e39cd8a2
ln613 <ln613@hotmail.com>
2018-01-20 12:31:59 -0800

    ass4 test getPaid

--- Lesson4/assignment/src/App.js
+++ Lesson4/assignment/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import PayrollContract from '../build/contracts/Payroll.json'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -44,25 +44,19 @@ class App extends Component {
      */
 
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const payroll = contract(PayrollContract)
+    payroll.setProvider(this.state.web3.currentProvider)
 
     // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    var payrollInstance
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
+      payroll.deployed().then((instance) => {
+        payrollInstance = instance
+        return payrollInstance.addFund(5)
       }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
+        return this.setState({ fund: result.c[0] })
       })
     })
   }
@@ -82,7 +76,7 @@ class App extends Component {
               <h2>Smart Contract Example</h2>
               <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
               <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <p>The stored value is: {this.state.fund}</p>
             </div>
           </div>
         </main>
--- Lesson4/assignment/test/payroll.js
+++ Lesson4/assignment/test/payroll.js
@@ -1,30 +1,53 @@
-var Payroll = artifacts.require("./Payroll.sol");
+const Payroll = artifacts.require("./Payroll.sol");
+
+const emptyAddress = '0x0000000000000000000000000000000000000000';
+const employeeAddress = web3.eth.accounts[1];
+//web3.personal.unlockAccount(employeeAddress, "password", 15000);
 
 contract('Payroll', accounts => {
 
-  it("...should add employ ...", () => Payroll.deployed()
+  it("...should add employee ...", () => Payroll.deployed()
     .then(x => {
-      x.addEmployee('0xc479f20b44276e99d949531a0bc0100b979563c6', 2);
-      return x.employees.call('0xc479f20b44276e99d949531a0bc0100b979563c6');
+      x.addEmployee(employeeAddress, 2);
+      return x.employees.call(employeeAddress);
     })
     .then(x => assert.equal(x[1].c[0], 20000))
   );
 
-  it("...should not add employ ...", () => Payroll.deployed()
-    .then(x => x.addEmployee('0xc479f20b44276e99d949531a0bc0100b979563c6', 2))
+  it("...should not add employee ...", () => Payroll.deployed()
+    .then(x => x.addEmployee(employeeAddress, 2))
     .catch(x => assert.isTrue(x instanceof Error))
   );
 
-  it("...should remove employ ...", () => Payroll.deployed()
-    .then(x => {
-      x.removeEmployee('0xc479f20b44276e99d949531a0bc0100b979563c6');
-      return x.employees.call('0xc479f20b44276e99d949531a0bc0100b979563c6');
-    })
-    .then(x => assert.equal(x[0], '0x0000000000000000000000000000000000000000'))
+  it("...should add fund ...", () => Payroll.deployed()
+    .then(x =>
+      x.addFund({ value: web3.toWei(10, "ether") })
+        .then(y => web3.eth.getBalance(x.address))
+    )
+    .then(x => assert.equal(web3.fromWei(x.valueOf()), 10))
   );
 
-  it("...should not remove employ ...", () => Payroll.deployed()
-    .then(x => x.addEmployee('0xc479f20b44276e99d949531a0bc0100b979563c6'))
+  it("...should getPaid ...", done => 
+    setTimeout(() => Payroll.deployed()
+      .then(x => x.getPaid({ from: employeeAddress })
+        .then(y => web3.eth.getBalance(x.address))
+      )
+      .then(x => assert.equal(web3.fromWei(x.valueOf()), 8))
+      .then(x => done()),
+      12000
+    )
+  );
+
+  it("...should remove employee ...", () => Payroll.deployed()
+    .then(x => {
+      x.removeEmployee(employeeAddress);
+      return x.employees.call(employeeAddress);
+    })
+    .then(x => assert.equal(x[0], emptyAddress))
+  );
+
+  it("...should not remove employee ...", () => Payroll.deployed()
+    .then(x => x.addEmployee(employeeAddress))
     .catch(x => assert.isTrue(x instanceof Error))
   );
 
