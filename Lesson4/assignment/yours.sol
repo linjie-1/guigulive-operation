40381835d91d5eb2fcfdfedd3780741a5a6c3ccf
xph <masterxph@gmail.com>
2018-01-20 10:17:30 +0000

    Add basic positive unit tests

--- Lesson4/assignment/yours.sol
+++ Lesson4/assignment/yours.sol
@@ -1,1 +1,36 @@
-/*作业请提交在这个目录下*/
+var Payroll = artifacts.require("./Payroll.sol");
+
+contract('Payroll', (accounts) => {
+
+  it("should add a valid employee with salary.", () => {
+    return Payroll.deployed().then((instance) => {
+      PayrollInstance = instance;
+
+      return PayrollInstance.addFund({value: web3.toWei(10)});
+    }).then(() => {
+      return PayrollInstance.addEmployee(accounts[1], 2);
+    }).then(() => {
+      return PayrollInstance.employees.call(accounts[1]);
+    }).then((employeeData) => {
+      assert.equal(employeeData[1].valueOf(), web3.toWei(2), "Employee not stored with salary 1.");
+    });
+  });
+
+  it("should remove an existing employee after paying owed salary.", () => {
+    return Payroll.deployed().then((instance) => {
+      PayrollInstance = instance;
+
+      return PayrollInstance.addFund({value: web3.toWei(10)});
+    }).then(() => {
+      return PayrollInstance.addEmployee(accounts[2], 2);
+    }).then(() => {
+      return PayrollInstance.employees.call(accounts[2]);
+    }).then((employeeData) => {
+       return PayrollInstance.removeEmployee(accounts[2]);
+    }).then(() => {
+      return PayrollInstance.employees.call(accounts[2]);
+    }).then((employeeData) => {
+       assert.equal(employeeData[0].valueOf(), 0x0, "Employee not removed.");
+    });
+  });
+});
