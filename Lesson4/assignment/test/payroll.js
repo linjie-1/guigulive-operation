7a1a219300443b95e7261feae6589ff8c49a55ea
StrangeCloud9 <BoWangCS@outlook.com>
2018-01-21 11:38:22 +0800

    lesson4

---
+++ Lesson4/assignment/test/payroll.js
@@ -0,0 +1,73 @@
+// - 在test文件夹中，写出对如下两个函数的单元测试：
+// - function addEmployee(address employeeId, uint salary) onlyOwner
+// - function removeEmployee(address employeeId) onlyOwner employeeExist(employeeId)
+
+var Payroll = artifacts.require("./Payroll.sol");
+
+contract('Payroll', function(accounts) {
+
+  it("Add employee", function() {
+    return Payroll.deployed().then(function(instance) {
+
+      payrollInstance = instance;
+      console.log('Start adding employee...');
+      payrollInstance.addEmployee(accounts[1], 1);
+      return payrollInstance.getEmployeeSalary(accounts[1]);
+
+    }).then((salary) => {
+      
+      assert.equal(salary.valueOf(), web3.toWei(1, 'ether'), 'Fail to add employee');
+
+    })
+  });
+
+  it("Add fund", function() {
+    var constractAddress;
+    return Payroll.deployed().then(function(instance) {
+
+      constractAddress = instance.address;
+      return instance.addFund({value:web3.toWei(10, 'ether')});
+
+    }).then((result) => {
+
+      balance = web3.eth.getBalance(constractAddress);
+      console.log(balance);
+      assert.equal(balance.valueOf(), web3.toWei(10, 'ether'), 'Fail to add fund');
+    
+    })
+  })
+
+  it("Get paid", function() {
+    var old_balance = 0;
+    return Payroll.deployed().then(function(instance) {
+
+      constractAddress = instance.address;
+      old_balance = web3.eth.getBalance(constractAddress).valueOf();
+      console.log(old_balance);
+      return instance.getPaid({from: accounts[1]});
+
+    }).then(function() {
+      
+      var new_balance = web3.eth.getBalance(constractAddress).valueOf();
+      console.log(new_balance);
+      var for_salary = web3.fromWei(old_balance - new_balance, 'ether');
+      console.log(for_salary);
+      assert.equal(1,for_salary,"Fail to get paid");
+
+    })
+  })
+
+  it("Remove employee", function() {
+    return Payroll.deployed().then(function(instance) {
+      
+      payrollInstance.removeEmployee(accounts[1]);
+      return payrollInstance.getEmployeeSalary(accounts[1]);
+
+    }).then((salary) => {
+
+      assert.equal(salary.valueOf(), 0, "Fail to remove employee");
+
+    })
+  });
+
+});
