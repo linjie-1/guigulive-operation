a14852e32cd656db20b32b0cd08d5acd234766c3
adug3 <m_fan@geishatokyo.com>
2018-01-21 12:08:06 +0900

    update testGetpaid

--- Lesson4/assignment/test/TestPayroll.sol
+++ Lesson4/assignment/test/TestPayroll.sol
@@ -8,13 +8,23 @@ import "../contracts/payroll.sol";
 contract TestPayroll is Payroll {
 
     address testAddress = 0xc42d7e5be797195e736683fd7624936f2537ed14;
-    
+    uint public initialBalance = 10 ether;
+
     function testAddEmployee() {
         addEmployee(testAddress, 1);
         var employee = employees[testAddress];
         Assert.equal(employee.id, testAddress, "employee should have correct address");
         uint expect = 1 ether;
         Assert.equal(employee.salary, expect, "employee should have correct salary");
+    }
+
+    function testGetPaid() {
+        var employee = employees[testAddress];
+        employee.lastPayday = now - payDuration;
+        uint oldBalance = testAddress.balance;
+        _getPaid(testAddress, now);
+        uint newBalance = testAddress.balance;
+        Assert.equal(newBalance - oldBalance, 1 ether, "employee should have correct payment");
     }
 
     function testRemoveEmployee() {
@@ -36,5 +46,4 @@ solidity 测试不能修改msg.sender，
 
 测试getPaid时因为timestamp无法直接修改，可以添加一个函数_getPaid带address和timestamp参数，
 通过测试来_getPaid实现曲线救国
-测试函数还没写
 */
