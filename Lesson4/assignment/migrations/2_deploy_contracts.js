73edbbed3b416fd8e02fe9ed52c33f36184b4c01
adug3 <m_fan@geishatokyo.com>
2018-01-21 11:26:47 +0900

    Revert "add lesson 4"
    This reverts commit 03080c97cb694bdca7e8e92f46a615d8aab9d02d.

--- Lesson4/assignment/migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Payroll = artifacts.require("./payroll.sol");
-
-
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-
-  deployer.deploy(Payroll);
-};
