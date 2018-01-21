7b312ab6259e8b25df2f7f3d730eb21624d4c677
Longbin Shen <slofslb@qq.com>
2018-01-21 22:19:54 +0800

    payroll的单元测试，用js实现

---
+++ Lesson4/assignment/payroll.js
@@ -0,0 +1,29 @@
+var Payroll = artifacts.require("./Payroll.sol");
+
+contract('Payroll', function(accounts) {
+
+  it("add employee ", function() {
+    return Payroll.deployed().then(function(instance) {
+      payrollInstance = instance;
+
+      return payrollInstance.addEmployee(accounts[1], 20, {from: accounts[0]});
+    }).then(function() {
+      return payrollInstance.isEmployeeExist.call(accounts[1]);
+    }).then(function(v) {
+      assert.equal(v, true, "the second employee should be exist");
+    });
+  });
+
+  it("remove employee ", function() {
+    return Payroll.deployed().then(function(instance) {
+      payrollInstance = instance;
+
+      return payrollInstance.removeEmployee(accounts[1], {from: accounts[0]});
+    }).then(function() {
+      return payrollInstance.isEmployeeExist.call(accounts[1]);
+    }).then(function(v) {
+      assert.equal(v, false, "the second employee should not be exist");
+    });
+  });
+
+});
