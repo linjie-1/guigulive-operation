fe8e54df8e9d984ba052bdebb7398c980131dfc6
Zidong <smillence1992@gmail.com>
2018-01-21 02:31:09 -0800

    test removeEmployee

--- Lesson4/test/payroll.js
+++ Lesson4/test/payroll.js
@@ -6,6 +6,7 @@ contract('Payroll', function(accounts) {
   const creator = accounts[0];
   const employeeId = accounts[1];
   const salary = 5;
+  const invalidSalary = -5;
 
   beforeEach(async function() {
     payroll = await Payroll.new({from: creator});
@@ -13,7 +14,7 @@ contract('Payroll', function(accounts) {
 
   it("addEmployee() test owner add new employee succeed", async function() {
     await checkEmployeeNotExist(employeeId);
-    let totalSalary = await payroll.totalSalary.call();
+    let totalSalary = await genTotalSalary();
     assert.equal(totalSalary, 0, "initial totalSalary should be 0");
     let result = await payroll.addEmployee(employeeId, salary, {from: creator});
     checkTransactionSucceed(result);
@@ -33,11 +34,11 @@ contract('Payroll', function(accounts) {
       parseInt(employeeData[2], 10),
       "LastPayday does not match"
     );
-    totalSalary = await payroll.totalSalary.call();
+    let updatedTotalSalary = await genTotalSalary();
     assert.equal(
-      parseInt(web3.fromWei(totalSalary, 'ether'), 10),
-      salary,
-      "totalSalary should be changed",
+      updatedTotalSalary,
+      totalSalary + salary,
+      "totalSalary should be increased",
     );
   });
 
@@ -45,6 +46,22 @@ contract('Payroll', function(accounts) {
     await payroll.addEmployee(employeeId, salary, {from: creator});
     await checkEmployeeExist(employeeId);
     let result = await payroll.addEmployee(employeeId, salary, {from: creator});
+    checkTransactionFail(result);
+  });
+
+  it("addEmployee() test owner add 0x0 address fail", async function() {
+    let result = await payroll.addEmployee(0x0, salary, {from: creator});
+    checkTransactionFail(result);
+  });
+
+  it("addEmployee() test owner add existing employee with invalid salary fail",
+    async function() {
+
+    let result = await payroll.addEmployee(
+      employeeId,
+      invalidSalary,
+      {from: creator},
+    );
     checkTransactionFail(result);
   });
 
@@ -71,6 +88,54 @@ contract('Payroll', function(accounts) {
     checkTransactionFail(result);
   });
 
+  it("addEmployee() test non-owner add 0x0 address fail", async function() {
+    let result = await payroll.addEmployee(0x0, salary, {from: employeeId});
+    checkTransactionFail(result);
+  });
+
+  it("removeEmployee() test owner remove existing employee succeed",
+    async function() {
+
+    await payroll.addEmployee(employeeId, salary, {from: creator});
+    await checkEmployeeExist(employeeId);
+    let totalSalary = await genTotalSalary();
+
+    let result = await payroll.removeEmployee(employeeId, {from: creator});
+    checkTransactionSucceed(result);
+    await checkEmployeeNotExist(employeeId);
+    let updatedTotalSalary = await genTotalSalary();
+    assert.equal(
+      updatedTotalSalary,
+      totalSalary - salary,
+      "totalSalary should be decreased",
+    );
+  });
+
+  it("removeEmployee() test owner remove non-existing employee fail",
+    async function() {
+
+    await checkEmployeeNotExist(employeeId);
+    let result = await payroll.removeEmployee(employeeId, {from: creator});
+    checkTransactionFail(result);
+  });
+
+  it("removeEmployee() test non-owner remove existing employee fail",
+    async function() {
+
+    await payroll.addEmployee(employeeId, salary, {from: creator});
+    await checkEmployeeExist(employeeId);
+    let result = await payroll.removeEmployee(employeeId, {from: employeeId});
+    checkTransactionFail(result);
+  });
+
+  it("removeEmployee() test non-owner remove non-existing employee fail",
+    async function() {
+
+    await checkEmployeeNotExist(employeeId);
+    let result = await payroll.removeEmployee(employeeId, {from: employeeId});
+    checkTransactionFail(result);
+  });
+
   async function checkEmployeeNotExist(employeeId) {
     let employeeData = await payroll.employees(employeeId);
     assert.equal(0x0, employeeData[0], "Employee should not exist");
@@ -90,6 +155,11 @@ contract('Payroll', function(accounts) {
 
   function checkTransactionSucceed(result) {
     assert.equal(0x01, result.receipt.status, "Transaction should succeed");
+  }
+
+  async function genTotalSalary() {
+    let totalSalary = await payroll.totalSalary.call();
+    return parseInt(web3.fromWei(totalSalary, 'ether'), 10);
   }
 
 });
