b22f7607177344c245629f3221fe1f9604410fee
Yammy <yammyxing@foxmail.com>
2018-01-21 17:01:22 +0800

    update test and add function to test getPaid

--- Lesson4/assignment/testPayroll.js
+++ Lesson4/assignment/testPayroll.js
@@ -2,6 +2,7 @@ var Payroll = artifacts.require("./Payroll.sol");
 
 contract('Payroll', function (accounts) {
 
+	// test addEmployee & removeEmployee
 	it("...should add and remove employee successfully.", function () {
 		return Payroll.deployed().then(function (instance) {
 			payrollInstance = instance;
@@ -11,22 +12,20 @@ contract('Payroll', function (accounts) {
 		}).then(function () {
 			return payrollInstance.addEmployee(accounts[2], 1);
 		}).then(function () {
-			assert(true, "pass the test.");
+			assert(true, "The employeeId should add and remove employee successfully.");
 		});
 	});
 
-	it("...the operator who execute addEmployee function should be owner.", function () {
+	it("...the operator who call addEmployee function should be owner.", function () {
 		return Payroll.deployed().then(function (instance) {
 			payrollInstance = instance;
-			return payrollInstance.addEmployee(accounts[1], 1, {
-				from: accounts[1]
-			});
+			return payrollInstance.addEmployee(accounts[1], 1, { from: accounts[1] });
 		}).catch(function (error) {
 			assert(error.toString().includes('revert'), "it should revert.")
 		})
 	})
 
-	it("...the employee added should not existed in employees.", function () {
+	it("...the employee added should not exist in employees.", function () {
 		return Payroll.deployed().then(function (instance) {
 			payrollInstance = instance;
 			return payrollInstance.addEmployee(accounts[1], 1);
@@ -37,20 +36,18 @@ contract('Payroll', function (accounts) {
 		})
 	})
 
-	it("...the operator who execute removeEmployee function should be owner.", function () {
+	it("...the operator who call removeEmployee function should be owner.", function () {
 		return Payroll.deployed().then(function (instance) {
 			payrollInstance = instance;
 			return payrollInstance.addEmployee(accounts[1], 1);
 		}).then(function () {
-			return payrollInstance.removeEmployee(accounts[1], {
-				from: accounts[1]
-			});
+			return payrollInstance.removeEmployee(accounts[1], { from: accounts[1] });
 		}).catch(function (error) {
-			assert(error.toString().includes('revert'), "it should revert.")
+			assert(error.toString().includes('invalid opcode'), "it should expect exception.")
 		})
 	})
 
-	it("...the employee removed should be existed in employees.", function () {
+	it("...the employee removed should exist in employees.", function () {
 		return Payroll.deployed().then(function (instance) {
 			payrollInstance = instance;
 			return payrollInstance.removeEmployee(accounts[1]);
@@ -59,4 +56,48 @@ contract('Payroll', function (accounts) {
 		})
 	})
 
+	// test getPaid
+	it("...the employee who get paid should exist in employees.", function () {
+		return Payroll.deployed().then(function (instance) {
+			payrollInstance = instance;
+			return payrollInstance.addEmployee(accounts[1], 1);
+		}).then(function () {
+			return payrollInstance.addMoney({ from: accounts[0], value: "100" });
+		}).then(function () {
+			return payrollInstance.employeeGetPaid({ from: accounts[2] })
+		}).catch(function (error) {
+			assert(error.toString().includes('invalid opcode'), "it should expect exception.")
+		})
+	})
+
+	it("...the employee who call getPaid should be himself", function () {
+		return Payroll.deployed().then(function (instance) {
+			payrollInstance = instance;
+			return payrollInstance.addEmployee(accounts[1], 1);
+		}).then(function () {
+			return payrollInstance.addMoney({ from: accounts[0], value: "100" });
+		}).then(function () {
+			return payrollInstance.employeeGetPaid({ from: accounts[0] })
+		}).catch(function (error) {
+			assert(error.toString().includes('invalid opcode'), "it should expect exception.")
+		})
+	})
+
+	it("...the employee should get paid successfully.", function () {
+		var previousBalance, nowBalance;
+		return Payroll.deployed().then(function (instance) {
+			payrollInstance = instance;
+			return payrollInstance.addEmployee(accounts[1], 1);
+		}).then(function () {
+			return payrollInstance.addMoney({ from: accounts[0], value: "100" });
+		}).then(function () {
+			return previousBalance = web3.eth.getBalance(accounts[1]);
+		}).then(function () {
+			return payrollInstance.employeeGetPaid({ from: accounts[1] })
+		}).then(function () {
+			return nowBalance = web3.eth.getBalance(accounts[1]);
+		}).then(function () {
+			assert(nowBalance > previousBalance, "employee account balance increase, get paid successfully.")
+		})
+	})
 });
