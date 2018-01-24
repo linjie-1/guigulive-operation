89708aa8055f0f3910cd5345fd25932825349359
adug3 <m_fan@geishatokyo.com>
2018-01-24 13:02:34 +0900

    remove hard code address

--- Lesson4/assignment/test/payroll.js
+++ Lesson4/assignment/test/payroll.js
@@ -5,11 +5,11 @@ contract('Payroll', function(accounts) {
         let payrollInstance;
         Payroll.deployed().then(function(instance) {
             payrollInstance = instance;
-            return payrollInstance.addEmployee("0xc42d7e5be797195e736683fd7624936f2537ed14", 1, {from: accounts[0]});
+            return payrollInstance.addEmployee(accounts[1], 1, {from: accounts[0]});
         }).then(function(result) {
-            return payrollInstance.employees.call("0xc42d7e5be797195e736683fd7624936f2537ed14");
+            return payrollInstance.employees.call(accounts[1]);
         }).then(function(result) {
-            assert.equal(result[0], "0xc42d7e5be797195e736683fd7624936f2537ed14", "employee.id is incorrect");
+            assert.equal(result[0], accounts[1], "employee.id is incorrect");
             assert.equal(result[1].toString(), "1000000000000000000", "employee.salary is incorrect");
             done();
         });
@@ -19,7 +19,7 @@ contract('Payroll', function(accounts) {
         let payrollInstance;
         Payroll.deployed().then(function(instance) {
             payrollInstance = instance;
-            return payrollInstance.addEmployee("0x99f3dda6472658b0a5fdd7d6482b220aeea5cf0c", 1, {from: "0xc42d7e5be797195e736683fd7624936f2537ed14"});
+            return payrollInstance.addEmployee(accounts[2], 1, {from: accounts[1]});
         }).catch(function(error) {
             assert.include(error.toString(), "revert", "contains 'revert'");
             done();
@@ -30,7 +30,7 @@ contract('Payroll', function(accounts) {
         let payrollInstance;
         Payroll.deployed().then(function(instance) {
             payrollInstance = instance;
-            return payrollInstance.addEmployee("0xc42d7e5be797195e736683fd7624936f2537ed14", 1, {from: accounts[0]});
+            return payrollInstance.addEmployee(accounts[1], 1, {from: accounts[0]});
         }).catch(function(error) {
             assert.include(error.toString(), "invalid opcode", "contains 'invalid opcode'");
             done();
@@ -41,9 +41,9 @@ contract('Payroll', function(accounts) {
         let payrollInstance;
         Payroll.deployed().then(function(instance) {
             payrollInstance = instance;
-            return payrollInstance.removeEmployee("0xc42d7e5be797195e736683fd7624936f2537ed14", {from: accounts[0]});
+            return payrollInstance.removeEmployee(accounts[1], {from: accounts[0]});
         }).then(function(result) {
-            return payrollInstance.employees.call("0xc42d7e5be797195e736683fd7624936f2537ed14");
+            return payrollInstance.employees.call(accounts[1]);
         }).then(function(result) {
             assert.equal(result[0], "0x0000000000000000000000000000000000000000", "employee.id is incorrect");
             assert.equal(result[1].toString(), "0", "employee.salary is incorrect");
@@ -55,7 +55,7 @@ contract('Payroll', function(accounts) {
         let payrollInstance;
         Payroll.deployed().then(function(instance) {
             payrollInstance = instance;
-            return payrollInstance.removeEmployee("0x99f3dda6472658b0a5fdd7d6482b220aeea5cf0c", {from: "0xc42d7e5be797195e736683fd7624936f2537ed14"});
+            return payrollInstance.removeEmployee(accounts[2], {from: accounts[1]});
         }).catch(function(error) {
             assert.include(error.toString(), "revert", "contains 'revert'");
             done();
@@ -66,7 +66,7 @@ contract('Payroll', function(accounts) {
         let payrollInstance;
         Payroll.deployed().then(function(instance) {
             payrollInstance = instance;
-            return payrollInstance.removeEmployee("0xc42d7e5be797195e736683fd7624936f2537ed14", {from: accounts[0]});
+            return payrollInstance.removeEmployee(accounts[1], {from: accounts[0]});
         }).catch(function(error) {
             assert.include(error.toString(), "invalid opcode", "contains 'invalid opcode'");
             done();
