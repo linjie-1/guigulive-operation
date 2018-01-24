0a49495e360055912db81bd3ac438ffd0679036a
dangfp <dangfeipeng@gmail.com>
2018-01-24 14:15:05 +0000

    提交第五课作业

---
+++ Lesson5/assignment/src/components/EditableCell.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { Input, Icon } from 'antd';
+
+class EditableCell extends React.Component {
+  state = {
+    value: this.props.value,
+    editable: false,
+  }
+  handleChange = (e) => {
+    const value = e.target.value;
+    this.setState({ value });
+  }
+  check = () => {
+    this.setState({ editable: false });
+    if (this.props.onChange) {
+      this.props.onChange(this.state.value);
+    }
+  }
+  edit = () => {
+    this.setState({ editable: true });
+  }
+  render() {
+    const { value, editable } = this.state;
+    return (
+      <div className="editable-cell">
+        {
+          editable ?
+            <div className="editable-cell-input-wrapper">
+              <Input
+                value={value}
+                onChange={this.handleChange}
+                onPressEnter={this.check}
+              />
+              <Icon
+                type="check"
+                className="editable-cell-icon-check"
+                onClick={this.check}
+              />
+            </div>
+            :
+            <div className="editable-cell-text-wrapper">
+              {value || ' '}
+              <Icon
+                type="edit"
+                className="editable-cell-icon"
+                onClick={this.edit}
+              />
+            </div>
+        }
+      </div>
+    );
+  }
+}
+
+export default EditableCell
---
+++ Lesson5/assignment/truffle.js
@@ -0,0 +1,11 @@
+module.exports = {
+  migrations_directory: "./migrations",
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      gas: 3000000,
+      network_id: "*" // Match any network id
+    }
+  }
+};
