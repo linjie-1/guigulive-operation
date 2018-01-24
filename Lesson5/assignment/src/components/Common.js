7814b704cb070ac50a0caf7eefad536e4c748389
Hans Wang <cwang25@ncsu.edu>
2018-01-24 10:57:06 +0000

    hw..5 ?

--- Lesson5/assignment/src/components/Common.js
+++ Lesson5/assignment/src/components/Common.js
@@ -16,21 +16,21 @@ class Common extends Component {
       }
     }
 
-    this.newFund = payroll.NewFund(updateInfo);
-    this.getPaid = payroll.GetPaid(updateInfo);
-    this.newEmployee = payroll.NewEmployee(updateInfo);
-    this.updateEmployee = payroll.UpdateEmployee(updateInfo);
-    this.removeEmployee = payroll.RemoveEmployee(updateInfo);
+    // this.newFund = payroll.NewFund(updateInfo);
+    // this.getPaid = payroll.GetPaid(updateInfo);
+    // this.newEmployee = payroll.NewEmployee(updateInfo);
+    // this.updateEmployee = payroll.UpdateEmployee(updateInfo);
+    // this.removeEmployee = payroll.RemoveEmployee(updateInfo);
 
     this.checkInfo();
   }
 
   componentWillUnmount() {
-    this.newFund.stopWatching();
-    this.getPaid.stopWatching();
-    this.newEmployee.stopWatching();
-    this.updateEmployee.stopWatching();
-    this.removeEmployee.stopWatching();
+    // this.newFund.stopWatching();
+    // this.getPaid.stopWatching();
+    // this.newEmployee.stopWatching();
+    // this.updateEmployee.stopWatching();
+    // this.removeEmployee.stopWatching();
   }
 
   checkInfo = () => {
