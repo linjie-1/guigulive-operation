25cf1f5c70929fbe232f0cbdd1c259ada1ba6e73
StrangeCloud9 <326902993@qq.com>
2018-01-24 14:31:22 +0000

    test

---
+++ Lesson5/assignment/truffle.js
@@ -0,0 +1,11 @@
+// See <http://truffleframework.com/docs/advanced/configuration>
+// to customize your Truffle configuration!
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
