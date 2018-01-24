6f3a6890e696cea558f4450cf7a952297ce188c3
sophiemarceauqu <sophiemarceau_qu@sina.com>
2018-01-24 08:30:22 +0000

    update
    update

--- Lesson4/assignment/migrations/2_deploy_contracts.js
+++ Lesson4/assignment/migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Ownable = artifacts.require("./Ownable.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
+var Payroll = artifacts.require("./payroll.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Ownable);
+  deployer.deploy(SafeMath);
+  deployer.link(Ownable, SafeMath, Payroll);
+  deployer.deploy(Payroll)
 };
