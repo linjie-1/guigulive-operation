50e644eb5216e341fedf80f25317247e48e6c83e
Zidong <smillence1992@gmail.com>
2018-01-23 23:54:09 -0800

    remove unused files

--- Lesson5/orgin/truffle-config.js
+++ Lesson5/orgin/truffle-config.js
@@ -1,10 +1,2 @@
 module.exports = {
-  migrations_directory: "./migrations",
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
 };
