3b3977690f2a4cb5b806978354a06e1e9888ae88
StrangeCloud9 <BoWangCS@outlook.com>
2018-01-26 23:15:22 +0800

    fix

--- Lesson5/assignment/payroll2/src/App.js
+++ Lesson5/assignment/payroll2/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-import PayRollContract from '../build/contracts/Payroll.json'
+
+import PayRollContract from '../build/contracts/PayRoll.json'
 import getWeb3 from './utils/getWeb3'
 
-import './css/oswald.css'
-import './css/open-sans.css'
-import './css/pure-min.css'
-import './App.css'
+
+import { Layout, Menu, Spin, Alert } from 'antd';
+
+import Employer from "./components/Employer";
+import Employee from "./components/Employee";
+
+import 'antd/dist/antd.css';
+import './App.css';
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   constructor(props) {
@@ -13,7 +20,9 @@ class App extends Component {
 
     this.state = {
       storageValue: 0,
-      web3: null
+      web3: null,
+      employerAccount:"",
+      mode:"employer"
     }
   }
 
@@ -36,61 +45,95 @@ class App extends Component {
   }
 
   instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
 
-   /* const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)*/
 
     const contract = require('truffle-contract')
-    const payRoll = contract(PayRollContract)
-    payRoll.setProvider(this.state.web3.currentProvider)
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var payRollInstance
+    const PayRoll = contract(PayRollContract)
+    PayRoll.setProvider(this.state.web3.currentProvider)
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      payRoll.deployed().then((instance) => {
-        payRollInstance = instance
 
-        // Stores a given value, 5 by default.
-        /*
-        return payRollInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return payRollInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })*/
-      })
+      console.log(accounts);
+
+      this.setState({
+        account:accounts[0],
+        employerAccount:accounts[0]
+      });
+
+
+      PayRoll.deployed().then((instance) => {
+        this.setState({
+          payroll:instance
+        });
+
+      });
     })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-        </nav>
+  onSelectTab = (obj) => {
+    console.log(obj)
+    this.setState({
+      mode: obj.key
+  });
+  }
 
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
-            </div>
-          </div>
-        </main>
-      </div>
+  onClickAddressLink=(address)=>{
+    this.setState({
+      mode:'employee',
+      account:address
+    });
+  }
+
+
+  renderContent = () => {
+    const { account, payroll, web3, mode,employerAccount } = this.state;
+
+    if (!payroll) {
+      return <Spin tip="Loading..." />;
+    }
+
+    switch(mode) {
+      case 'employer':
+        return <Employer account={employerAccount} payroll={payroll} web3={web3} onClickAddressLink={this.onClickAddressLink} />
+      case 'employee':
+        return <Employee account={account} payroll={payroll} web3={web3} />
+      default:
+        return <Alert message="请选一个模式" type="info" showIcon />
+    }
+  }
+
+
+  render() {
+    const {mode}=this.state;
+    let selkey=[];
+    selkey.push(mode);
+    console.log("mode:",mode);
+    return (
+      <Layout>
+        <Header className="header">
+          <div className="logo">老董区块链干货铺员工系统</div>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            selectedKeys={selkey}
+            defaultSelectedKeys={['employer']}
+            style={{ lineHeight: '64px' }}
+            onSelect={this.onSelectTab}
+          >
+            <Menu.Item key="employer">雇主</Menu.Item>
+            <Menu.Item key="employee">雇员</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Layout style={{ padding: '24px 0', background: '#fff', minHeight: '600px' }}>
+            {this.renderContent()}
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Payroll ©2018 老董区块链干货铺
+        </Footer>
+      </Layout>
     );
   }
 }
