56e24099bdc612411704d761fa17b87b1f2eb8ba
Jialiang <wangvincent1993@gmail.com>
2018-01-29 23:25:17 +0000

    hw

---
+++ Lesson5/assignment/truffle.js
@@ -0,0 +1,12 @@
+module.exports = {
+  migrations_directory: "./migrations",
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      gas: 3000000,
+      network_id: "*" // Match any network id
+    }
+  }
+};
+
