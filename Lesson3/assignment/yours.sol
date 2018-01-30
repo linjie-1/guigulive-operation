803b3294b9f4a702fe34df38340fe601a69b42a5
韩博文 <hanbowen@meituan.com>
2018-01-30 12:53:26 +0800

    修改第三课作业格式

--- Lesson3/assignment/yours.sol
+++ Lesson3/assignment/yours.sol
@@ -1,122 +1,83 @@
-/*作业请提交在这个目录下*/
-//新增更改员工薪水的支付地址的函数
-function changePaymentAddress(address employeeId, address newEmployeeId) onlyOwner employeeExist(employeeId) {
-  var employee = employees[employeeId];
-  
-  employees[employeeId].id = newEmployeeId;
-}
-
-//加分题
-L[O] = 0
-
-L[A] = A + merge[L[0],0] 
-     = [A,0]
-
-L[B] = B + merge[L[0],0] 
-     = [B,0]
-
-L[C] = C + merge[L[0],0] 
-     = [C,0]
-
-L[K1] = K1 + merge[L[B],L[A],B,A]
-      = K1 + merge[[B,0],[A,0],[B,A]]
-      = [K1,B] + merge[[0],[A,0],[A]]
-      = [K1,B,A,0]
-      
-L[K2] = K2 + merge[L[C],L[A],C,A]
-      = K2 + merge[[C,0]],[A,0]],[C,A]]
-      = [K2,C,A,0]
-
-L[Z] = Z + merge[L[K2],L[K1],[K2,K1]]     
-     = Z + merge[[K2,C,A,0],[K1,B,A,0],[K2,K1]]     
-     = [Z,K2] + merge[[C,A,0],[B,A,0],K1]
-     = [Z,K2,K1] + merge[[C,A,0],[B,A,0]]
-     = [Z,K2,K1,C,A,B,0]
-
-
-//源码
-pragma solidity ^0.4.14;
-
+pragma solidity ^ 0.4.14;
+import "./SafeMath.sol";
 contract Payroll {
+    using SafeMath for uint;
     struct Employee {
         address id;
         uint salary;
         uint lastPayday;
     }
-    
     uint constant payDuration = 10 seconds;
-    
-    uint totalSalary;
     address owner;
-    mapping(address => Employee) public employees;
+    mapping (address => Employee) employees;
 
-    function Payroll() {
+    uint totalSalary = 0;
+    
+    function Payroll() public  {
         owner = msg.sender;
     }
     
-    modifier onlyOwner {
+    function _partialPaid(Employee employee) private {
+        uint payment = employee.salary.mul((now.sub(employee.lastPayday)).div(payDuration)) ;
+        if(payment > 0){
+            employee.lastPayday = now;
+            employee.id.transfer(payment);
+        }
+    }
+    
+
+    modifier requireOwner() {
         require(msg.sender == owner);
         _;
     }
     
-    modifier employeeExist(address employeeId) {
-         var employee = employees[employeeId];
-        assert(employee.id != 0x0);
+    modifier validAddress(address addr) {
+        require(addr != 0x0);
         _;
     }
     
-    function _partialPaid(Employee employee) private {
-        uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
-        employee.id.transfer(payment);
+    function addEmployee(address employeeId, uint salary) public requireOwner
+                validAddress(employeeId){
+        var e = employees[employeeId];
+        if (e.id != 0x0) revert();
+        totalSalary = totalSalary.add(salary);
+        employees[employeeId] = Employee(employeeId, salary, now);
     }
     
-    function addEmployee(address employeeId, uint salary) onlyOwner {
-        var employee = employees[employeeId];
-        assert(employee.id == 0x0);
-        totalSalary += salary * 1 ether;
-        
-        employees[employeeId] = Employee(employeeId, salary * 1 ether, now);
+    function removeEmployee(address employeeId) public payable requireOwner validAddress(employeeId){
+        var e = employees[employeeId];
+        if (e.id == 0x0) revert();
+        _partialPaid(e);
+        totalSalary-= e.salary;
     }
     
-    function removeEmployee(address employeeId) onlyOwner employeeExist(employeeId) {
-        
-        var employee = employees[employeeId];
-        
-        _partialPaid(employee);
-        totalSalary -= employees[employeeId].salary;
-        delete employees[employeeId];
-        
+    function updateEmployee(address employeeId, uint salary) public requireOwner payable validAddress(employeeId){
+        var e = employees[employeeId];
+        if (e.id == 0x0) revert();
+        e.salary = salary;
     }
-    
-    function updateEmployee(address employeeId, uint salary) onlyOwner employeeExist(employeeId) {
-        var employee = employees[employeeId];
-        
-        _partialPaid(employee);
-        totalSalary -= employees[employeeId].salary;
-        employees[employeeId].salary = salary * 1 ether;
-        totalSalary += employees[employeeId].salary;
-        employees[employeeId].lastPayday = now;
-    }
-    
-    function addFund() payable returns (uint) {
+
+
+    function addFund() public payable returns(uint) 
+    {
         return this.balance;
     }
     
-    function calculateRunway() returns (uint) {
-        return this.balance / totalSalary;
+    
+    
+    function calculateRunway() public view returns(uint) {
+       return totalSalary;
     }
     
-    function hasEnoughFund() returns (bool) {
+    function hasEnoughFund() public  view returns(bool) {
         return calculateRunway() > 0;
     }
     
-    function getPaid() employeeExist(msg.sender) {
-        var employee = employees[msg.sender];
+    function getPaid(Employee e) internal requireOwner {
         
-        uint nextPayday = employee.lastPayday + payDuration;
+        var nextPayday = e.lastPayday.add(payDuration);
         assert(nextPayday < now);
-        
-        employees[msg.sender].lastPayday = nextPayday;
-        employee.id.transfer(employee.salary);
+        e.lastPayday = nextPayday;
+        e.id.transfer(e.salary);
     }
 }
