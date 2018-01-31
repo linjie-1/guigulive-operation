bc579c8fa8bd8f122d6596518090305dc04574f5
liuliang <iliang531@gmail.com>
2018-01-31 15:16:37 +0800

    提交作业..

--- Lesson6/orgin/migrations/2_deploy_contracts.js
+++ Lesson6/orgin/migrations/2_deploy_contracts.js
@@ -1,9 +1,12 @@
-var Ownable = artifacts.require("./lab/Ownable.sol");
-var SafeMath = artifacts.require("./lab/SafeMath.sol");
+var Ownable = artifacts.require("./Ownable.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
 var Payroll = artifacts.require("./Payroll.sol");
+
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
   deployer.deploy(SafeMath);
+
   deployer.link(Ownable, Payroll);
+  deployer.link(SafeMath, Payroll);
   deployer.deploy(Payroll);
 };
