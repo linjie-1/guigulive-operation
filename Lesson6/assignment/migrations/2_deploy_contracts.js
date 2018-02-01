91ddda4d68bbbf17ee99ea7c61e067c2a6a86384
alanwang123 <[1727181@gmail.com]>
2018-02-01 09:46:06 +0800

    lesson6

---
+++ Lesson6/assignment/migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Ownable = artifacts.require("./Ownable.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
+var Payroll = artifacts.require("./Payroll.sol");
+
+
+module.exports = function(deployer) {
+  // deployer.deploy(SimpleStorage);
+  deployer.deploy(Ownable);
+  deployer.deploy(SafeMath);
+  deployer.link(Ownable,Payroll);
+  deployer.link(SafeMath,Payroll);
+  deployer.deploy(Payroll);
+
+};
